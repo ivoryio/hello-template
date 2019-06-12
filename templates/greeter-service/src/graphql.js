16b5b8f47bebe6550eed5a109df5926cb6dc2276
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-06-12 16:01:24 +0300

    handle correct fields i.e addGreeting instead of createGreeting

--- templates/greeter-service/src/graphql.js
+++ templates/greeter-service/src/graphql.js
@@ -4,12 +4,11 @@ const retrieveAllGreetings = require('./repository/retrieveAllGreetings')
 const getGreeting = require('./usecases/getGreeting')
 const createGreeting = require('./usecases/createGreeting')
 
-
 exports.resolver = async (event) => {
   switch(event.field) {
     case 'greeting':
       return await getGreeting(retrieveAllGreetings)(event.arguments.name)
-    case 'createGreeting':
+    case 'addGreeting':
       return await createGreeting(persistGreeting)(event.arguments.text)
   }
 }
