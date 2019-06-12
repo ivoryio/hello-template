edc19a34ff0cae9503d1466c40a03b483a9a0fa3
Bogdan Balan <bogdan.balan@thinslices.com>
2019-06-12 09:33:53 +0300

    Add infrastructure resources and usecases
    Signed-off-by: Bogdan Balan <bogdan.balan@thinslices.com>

---
+++ templates/greeter-service/src/rest.js
@@ -0,0 +1,33 @@
+const cors = require('cors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const awsServerlessExpress = require('aws-serverless-express')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+const getGreeting = require('./usecases/getGreeting')
+const createGreeting = require('./usecases/createGreeting')
+
+const app = express()
+const router = express.Router()
+
+router.use(cors())
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(awsServerlessExpressMiddleware.eventContext())
+
+router.get('/greeting', async (req, res) => {
+  const greeting = await getGreeting()(req.body.name)
+  res.status(200).json({ greeting })
+})
+
+router.post('/greeting', async (req, res) => {
+  const greeting = await createGreeting()(req.body.name)
+  res.status(201).json({ greeting })
+})
+
+app.use('/', router)
+
+const server = awsServerlessExpress.createServer(app)
+
+exports.handler = (event, context) =>
+  awsServerlessExpress.proxy(server, event, context)
---
+++ templates/greeter-service/src/usecases/getGreeting.js
@@ -0,0 +1,15 @@
+module.exports = () =>
+  async function(name) {
+    try {
+      const greetings = ['Hello', 'Hey', 'Salut']
+      const greeting = greetings[getRandomInt(greetings.length)]
+
+      return `${greeting} name`
+    } catch (err) {
+      throw err
+    }
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max))
+    }
+  }
