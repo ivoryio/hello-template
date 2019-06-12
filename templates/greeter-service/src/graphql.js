a53f56b4e840c876fcc6268a7c278d0cf55b40e3
Bogdan Balan <bogdan.balan@thinslices.com>
2019-06-12 13:29:58 +0300

    Update resolve to pass repository functions to usecases
    Signed-off-by: Bogdan Balan <bogdan.balan@thinslices.com>

--- templates/greeter-service/src/graphql.js
+++ templates/greeter-service/src/graphql.js
@@ -1,11 +1,15 @@
+const persistGreeting = require('./repository/persistGreeting')
+const retrieveAllGreetings = require('./repository/retrieveAllGreetings')
+
 const getGreeting = require('./usecases/getGreeting')
 const createGreeting = require('./usecases/createGreeting')
+
 
 exports.resolver = async (event) => {
   switch(event.field) {
     case 'greeting':
-      return await getGreeting()(event.arguments.name)
+      return await getGreeting(retrieveAllGreetings)(event.arguments.name)
     case 'createGreeting':
-      return await createGreeting()(event.arguments.greeting)
+      return await createGreeting(persistGreeting)(event.arguments.text)
   }
 }
