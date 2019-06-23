054a96e32ecb42fd02d00d562ed59c5ab8f5149f
Bogdan Balan <bogdan.balan@thinslices.com>
2019-06-23 18:12:11 +0300

    Refactor web pipeline and web project to properly build
    Signed-off-by: Bogdan Balan <bogdan.balan@thinslices.com>

---
+++ templates/web/configureEnv.js
@@ -0,0 +1,78 @@
+const AWS = require('aws-sdk')
+const shell = require('shelljs')
+
+var ssm = new AWS.SSM({ apiVersion: '2014-11-06', region: 'us-east-1' })
+
+const appParameters = [
+  {
+    ssm: `ivory-project-region`,
+    react: [`REGION_PLACEHOLDER`, 'APPSYNC_REGION_PLACEHOLDER']
+  },
+  {
+    ssm: `user-service-pool-id-${process.env.ENVIRONMENT}`,
+    react: ['USER_POOL_ID_PLACEHOLDER']
+  },
+  {
+    ssm: `user-service-web-client-id-${process.env.ENVIRONMENT}`,
+    react: ['USER_POOL_WEB_CLIENT_ID_PLACEHOLDER']
+  },
+  {
+    ssm: `data-gateway-url-${process.env.ENVIRONMENT}`,
+    react: ['APPSYNC_GRAPHQL_ENDPOINT_PLACEHOLDER']
+  }
+]
+
+validateEnvironment()
+  .then(retrieveSSMParameters)
+  .then(configureReactParameters)
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
+
+function validateEnvironment() {
+  const ENVIRONMENT = process.env.ENVIRONMENT
+  if (ENVIRONMENT !== 'staging' && ENVIRONMENT !== 'production') {
+    return Promise.reject(
+      `Expecting ENVIRONMENT to be staging or production, got: ${
+        process.env.ENVIRONMENT
+      }`
+    )
+  }
+
+  return Promise.resolve()
+}
+
+function retrieveSSMParameters() {
+  const params = {
+    Names: appParameters.map(p => p.ssm)
+  }
+
+  return ssm
+    .getParameters(params)
+    .promise()
+    .then(data => {
+      if (data.InvalidParameters.length > 0) {
+        return Promise.reject(
+          `Invalid parameters: ${data.InvalidParameters.join(' ; ')}`
+        )
+      }
+
+      return data.Parameters
+    })
+}
+
+function configureReactParameters(ssmParameters) {
+  ssmParameters.forEach(ssmParameter => {
+    const appParam = appParameters.find(p => p.ssm === ssmParameter.Name)
+
+    appParam.react.forEach(reactParam =>
+      shell.sed(
+        '-i',
+        reactParam,
+        ssmParameter.Value,
+        `.env.${process.env.ENVIRONMENT}`
+      )
+    )
+  })
+}
