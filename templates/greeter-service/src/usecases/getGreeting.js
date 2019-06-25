4bcc209e9c8e5c37717c0c73780b6043205710d2
Bogdan Balan <bogdan.balan@thinslices.com>
2019-06-25 09:50:53 +0300

    Handle case were there is no greeting in the DB

--- templates/greeter-service/src/usecases/getGreeting.js
+++ templates/greeter-service/src/usecases/getGreeting.js
@@ -2,15 +2,32 @@ module.exports = retrieveAllGreetings =>
   async function(name) {
     try {
       const greetings = await retrieveAllGreetings()
-      const greeting = greetings[getRandomInt(greetings.length)]
-      return {
-        id: greeting.id,
-        text:`${greeting.text} ${name ? name : 'John Doe'}`}
+
+      const noGreetings = greetings.length === 0
+      if (noGreetings) {
+        return defaultGreeting(name)
+      }
+      return randomGreeting(greetings, name)
     } catch (err) {
       throw err
     }
-
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max))
-    }
   }
+
+function defaultGreeting(name) {
+  return {
+    id: '',
+    text: `Hello ${name}`
+  }
+}
+
+function randomGreeting(greetings, name) {
+  const greeting = greetings[getRandomInt(greetings.length)]
+  return {
+    id: greeting.id,
+    text: `${greeting.text} ${name ? name : 'John Doe'}`
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
